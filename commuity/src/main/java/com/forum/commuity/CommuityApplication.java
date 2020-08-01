f526b1f022b32a51fead04ffa2fdb5f43c0d88c3
writeboy <59385072+writeboy@users.noreply.github.com>
2020-08-01 08:03:24 +0800

    commuity commit

---
+++ commuity/src/main/java/com/forum/commuity/CommuityApplication.java
@@ -0,0 +1,13 @@
+package com.forum.commuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuityApplication.class, args);
+    }
+
+}
---
+++ commuity/src/test/java/com/forum/commuity/CommuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.commuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
